a2b2f38e4c5546c5a9d385059901094e399aeb40
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-06-24 22:58:15 +0530

    Time: 84 ms (5.05%) | Memory: 286.1 MB (5.00%) - LeetSync

--- is-subsequence/is-subsequence.cpp
+++ is-subsequence/is-subsequence.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    bool solve(string s, string t, int m, int n){
+        if(m == 0){
+            return true;
+        }
+
+        if(n == 0){
+            return false;
+        }
+
+        if(s[m-1] == t[n-1]){
+            return solve(s, t, m-1, n-1);
+        }
+        else{
+            return solve(s, t, m, n-1);
+        }  
+        
+    }
     bool isSubsequence(string s, string t) {
 
-        int i=0, j=0;
-        while(i<s.size() && j<t.size()){
-            if(s[i] == t[j]){
-                i++;
-                j++;
-            }else{
-                j++;
-            }
-        } 
-        return i==s.size() ? 1:0;
-
+        int m = s.size();
+        int n = t.size();
+        
+        return solve(s, t, m, n);
     }
 };
