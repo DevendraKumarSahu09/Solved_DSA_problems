380be5831ef28cdb0c8009359e7d9b9cfa1686ec
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-06-26 23:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (50.69%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -2,37 +2,29 @@ class Solution {
 public:
     int solve(int row, int col, vector<vector<int> > &dp){
 
-        if(row<0 || col<0){
-            return 0;
-        }
+        if(row<0 || col<0) return 0;
+        
+        if(row == 0 && col == 0) return 1;
 
-        if(row == 0 && col == 0){
-            return 1;
-        }
-
-        if(dp[row][col] != -1){
-            return dp[row][col];
-        }
+        if(dp[row][col] != -1) return dp[row][col];
 
         int up=0, left=0;
 
-        if (row - 1 >= 0) {
+        if (row - 1 >= 0) 
             if (dp[row - 1][col] != -1) {
                 up = dp[row - 1][col];
             } else {
                 up = solve(row - 1, col, dp);
                 dp[row - 1][col] = up;
             }
-        }
 
-        if (col - 1 >= 0) {
+        if (col - 1 >= 0) 
             if (dp[row][col - 1] != -1) {
                 left = dp[row][col - 1];
             } else {
                 left = solve(row, col - 1, dp);
                 dp[row][col - 1] = left;
             }
-        }
 
         dp[row][col] = up+left;
         return dp[row][col];
