923a2c8996eeac3f0cf24ca72e622b868972df47
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-07-27 22:49:06 +0530

    Time: 2 ms (43.81%) | Memory: 5.9 MB (74.72%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+        int ans = sqrt(num);
+
+        if(ans * ans == num) 
+            return true;
+        else
+            return false;
+    }
+};
