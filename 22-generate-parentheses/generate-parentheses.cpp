89b541b585e54bb9e6a35a3b4b367bece4630fd7
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-08-05 23:00:32 +0530

    Time: 167 ms (5.04%) | Memory: 218.6 MB (5.02%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+            if (ch == '(' ) {
+                st.push(ch);
+            } else {
+                if (!st.empty()) {
+                    char checkTop = st.top();
+                    if (ch == ')' && checkTop == '(') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false; 
+                }
+            }
+        }
+        return st.empty(); 
+    }
+
+    void solve(vector<string>& ans, string s, int n) {
+        if (s.size() >= n) {
+            if (isValid(s)) {
+                ans.push_back(s);
+            }
+            return; 
+        }
+
+        solve(ans, s + "(", n);
+        solve(ans, s + ")", n);
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s = "";
+        solve(ans, s, 2 * n);
+        return ans;
+    }
+};
