4b2cd9e72cd6b5f7e8fddec7ed4bb3fc83fc84fc
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-08-12 21:45:24 +0530

    Update build-array-from-permutation.cpp

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> buildArray(vector<int>& nums) {
         
         vector<int>ans;
-
         for(int i=0; i<nums.size(); i++){
             int value = nums[nums[i]];
             ans.push_back(value);
