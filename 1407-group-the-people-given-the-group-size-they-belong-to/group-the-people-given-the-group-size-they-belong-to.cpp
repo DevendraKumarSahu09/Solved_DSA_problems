f7bc8300bf7ab97bf6db05ca65f6c6170cab3c21
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2023-09-11 22:17:59 +0530

    Time: 4 ms (95.05%) | Memory: 12.6 MB (80.59%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,25 +1,52 @@
+// class Solution {
+// public:
+//     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        
+//         vector<vector<int>>ans;
+//         vector<pair<int, int>>store;
+//         int n = groupSizes.size();
+
+//         for(int i=0; i<n; i++){
+//             store.push_back(make_pair(groupSizes[i], i));
+//         }
+
+//         sort(store.begin(), store.end());
+
+//         vector<int>currGrp;
+//         for(int i=0; i<n; i++){
+
+//             currGrp.push_back(store[i].second);
+
+//             if(currGrp.size() == store[i].first){
+//                 ans.push_back(currGrp);
+//                 currGrp.clear();
+//             }
+//         }
+
+//         return ans;
+     
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         
         vector<vector<int>>ans;
-        vector<pair<int, int>>store;
+        unordered_map<int, vector<int>> store;
+
         int n = groupSizes.size();
 
         for(int i=0; i<n; i++){
-            store.push_back(make_pair(groupSizes[i], i));
-        }
 
-        sort(store.begin(), store.end());
+            store[groupSizes[i]].push_back(i);
 
-        vector<int>currGrp;
-        for(int i=0; i<n; i++){
+            if(store[groupSizes[i]].size() == groupSizes[i]){
 
-            currGrp.push_back(store[i].second);
-
-            if(currGrp.size() == store[i].first){
-                ans.push_back(currGrp);
-                currGrp.clear();
+                ans.push_back(store[groupSizes[i]]);
+                store[groupSizes[i]].clear();
+                
             }
         }
 
